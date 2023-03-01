21b406e084c7ac4ada53b72925d8451e295f2834
SSuSG <psg980331@naver.com>
2023-03-02 00:33:20 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,025 ms, Memory: 222,304 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,83 +1,75 @@
 import java.io.*;
 import java.util.*;
+
 public class Solution {
-	static int v,e;
-	static Edge[] edges;
-	static int[] p;
-	
-	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("res/input_d4_치즈.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	 public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
-        
-        for (int tc = 1; tc <= t; tc++) {
-        	st = new StringTokenizer(br.readLine());
-			v = Integer.parseInt(st.nextToken());
-			e = Integer.parseInt(st.nextToken());
-			
-			edges = new Edge[e];
-			for (int i = 0; i < e; i++) {
-				st = new StringTokenizer(br.readLine());
-				int from = Integer.parseInt(st.nextToken());
-				int to = Integer.parseInt(st.nextToken());
-				long weight = Integer.parseInt(st.nextToken());
-				edges[i] = new Edge(from,to,weight);
-			}
-			
-			Arrays.sort(edges);
-			make();
-			long result =0;		//최소 가중치
-			int cnt = 0; 		//cnt == N-1이 되면 break
-			for (Edge edge : edges) {
-				if(union(edge.from,edge.to)) {
-					result += edge.weight;
-					if(++cnt == v-1)
-						break;
-				}
-			}
-			
-			sb.append("#" + tc + " " + result + "\n");
-		}
-        System.out.println(sb);
-	}
-	
-	static void make() {
-		p = new int[v+1];
-		for (int i = 1; i <= v; i++) {
-			p[i] = i;
-		}
-	}
-	
-	static int find(int a) {
-		if(p[a] == a)
-			return a;
-		return p[a] = find(p[a]);
-	}
-	
-	static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if(aRoot == bRoot) return false;
-		p[bRoot] = aRoot;
-		return true;
-	}
-	
-	static class Edge implements Comparable<Edge>{
-		int from;
-		int to;
-		long weight;
-		
-		public Edge(int from, int to, long weight) {
-			this.from = from;
-			this.to = to;
-			this.weight = weight;
-		}
 
-		@Override
-		public int compareTo(Edge o) {
-			return (int) (this.weight - o.weight);
-		}
-	}
+        for (int tc = 1; tc <= t; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            
+            ArrayList<Node>[] list = new ArrayList[n+1];
+            boolean[] v = new boolean[n+1];
+
+            for(int i = 0 ; i <= n; i++) {
+                list[i] = new ArrayList<>();
+            }
+
+            for (int i = 0; i < e; i++) {
+                st = new StringTokenizer(br.readLine());
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                int weight = Integer.parseInt(st.nextToken());
+                list[from].add(new Node(to,weight));
+                list[to].add(new Node(from,weight));
+            }
+
+            long result = 0; int cnt =0;
+            PriorityQueue<Node> pq = new PriorityQueue<>();
+            pq.offer(new Node(1,0)); 	//정점,가중치
+
+            while(!pq.isEmpty()) {
+                Node cur = pq.poll();
+
+                if(v[cur.v]) continue;
+
+                v[cur.v] = true;
+                result += cur.weight;
+
+                if(cnt++ == n-1)
+                    break;
+
+                //가장 가까운 정점에서 주변으로 가는값들 최소값으로 초기화
+                for(int i = 0; i < list[cur.v].size(); i++) {
+                    Node next = list[cur.v].get(i);
+                    if(v[next.v] == true) 
+                        continue;
+                    pq.add(next);
+                }
+            }
+            sb.append("#" + tc + " " + result + "\n");
+        }
+        System.out.println(sb);
+    }
+    
+    static class Node implements Comparable<Node>{
+        int v;
+        int weight;
+
+        public Node(int x, int weight) {
+            this.v = x;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.weight , o.weight);
+        }
+
+    }
 }
