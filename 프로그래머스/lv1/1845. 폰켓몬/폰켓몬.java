11b72d58dd1be2e68431d235d2368cf1647f9ac6
SSuSG <psg980331@naver.com>
2023-03-16 14:00:20 +0900

    [level 1] Title: 폰켓몬, Time: 3.82 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            map.put( nums[i] , map.getOrDefault(nums[i],0)+1);
+        }
+        if(map.size() >= nums.length/2)
+            answer = nums.length/2;
+        else
+            answer = map.size();
+
+        return answer;
+    }
+}
