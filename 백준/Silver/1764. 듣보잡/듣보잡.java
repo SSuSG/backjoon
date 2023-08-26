cef11101228af87b8194f475c542d2fd9354c7be
SSuSG <psg980331@naver.com>
2023-08-26 23:50:25 +0900

    [Silver IV] Title: 듣보잡, Time: 392 ms, Memory: 30516 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        HashSet<String> notSeeHuman = new HashSet<>();
+        ArrayList<String> notSeeNotHearHuman = new ArrayList<>();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        String human = "";
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            human = st.nextToken();
+            notSeeHuman.add(human);
+        }
+
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            human = st.nextToken();
+            if(notSeeHuman.contains(human)){
+                notSeeNotHearHuman.add(human);
+            }
+        }
+
+        Collections.sort(notSeeNotHearHuman);
+        System.out.println(notSeeNotHearHuman.size());
+        for (int i = 0; i < notSeeNotHearHuman.size(); i++) {
+            System.out.println(notSeeNotHearHuman.get(i));
+        }
+
+    }
+}
