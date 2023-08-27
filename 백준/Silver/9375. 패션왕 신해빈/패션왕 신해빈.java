a031992d7031ff140cda9f18b520e7d1352b54bb
SSuSG <psg980331@naver.com>
2023-08-27 23:48:12 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 92 ms, Memory: 11844 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuffer sb = new StringBuffer();
+        int t = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < t; i++) {
+            int result = 1;
+            HashMap<String, Integer> map = new HashMap<>();
+            st = new StringTokenizer(br.readLine());
+            int c = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < c; j++) {
+                st = new StringTokenizer(br.readLine());
+                String temp1 = st.nextToken();
+                String temp2 = st.nextToken();
+
+                map.put(temp2, map.getOrDefault(temp2 , 0)+1);
+            }
+
+
+            for (String key : map.keySet()) {
+                int num = map.get(key);
+                result *= (num+1);
+            }
+            
+           sb.append(result-1+"\n");
+        }
+        System.out.println(sb);
+
+    }
+}
